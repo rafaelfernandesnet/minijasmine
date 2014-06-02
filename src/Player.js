63074e52e1efd88790fb6ea167ebbf3ee11abe62
RAFAEL FERNANDES <rafael.net.ca@gmail.com>
2014-06-02 13:34:49 -0400

    Initialize

---
+++ src/Player.js
@@ -0,0 +1,28 @@
+function Player() {
+	this.djName = "Rafael";
+}
+
+Player.prototype.play = function(song) {
+  this.currentlyPlayingSong = song;
+  this.isPlaying = true;
+};
+
+Player.prototype.pause = function() {
+  this.isPlaying = false;
+};
+
+Player.prototype.resume = function() {
+  if (this.isPlaying) {
+    throw new Error("song is already playing");
+  }
+
+  this.isPlaying = true;
+};
+
+Player.prototype.makeFavorite = function() {
+  this.currentlyPlayingSong.persistFavoriteStatus(true);
+};
+
+Player.prototype.makeItHappen = function(value) {
+  return value;
+};
